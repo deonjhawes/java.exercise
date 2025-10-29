05ecdc72e465cb2a77d8b8aad1aec08bb1a44c09
deonjhawes <deonjhawes@gmail.com>
2025-10-29 12:13:46 +0200

    hiiiiiiiiiiiiiiiiii

---
+++ basics.js
@@ -0,0 +1,14 @@
+// TODO: Create a variable called firstName and store your first name in it
+const FirstName = "Deon"
+// TODO: Create a variable called lastName and store your last name in it
+const LastName= "Hawes"
+// TODO: Create a variable called age using let (you'll modify this later)
+let age= "20"
+// TODO: Create a fullName variable that combines firstName and lastName
+const fullName= FirstName +" "+ LastName
+// TODO: Print a greeting to the console using these variables
+console.log("HELLO Hiiiii, my name is " + fullName + " and i am "+ age +" years old!!! ")
+// TODO: Try changing the age variable to a different value
+age="21"
+console.log("i will be " + age + " next year")
+
